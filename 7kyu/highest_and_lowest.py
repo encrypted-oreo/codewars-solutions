6f67b43012899184ef4508056ef3bed7b58cf29f
encrypted-oreo <181586123+encrypted-oreo@users.noreply.github.com>
2024-10-20 17:01:33 +0100

    Create highest_and_lowest.py

---
+++ 7kyu/highest_and_lowest.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/554b4ac871d6813a03000035/solutions/python
+
+def high_and_low(numbers):
+    nums = list(map(int, numbers.split(" ")))
+    return f"{max(nums)} {min(nums)}"
