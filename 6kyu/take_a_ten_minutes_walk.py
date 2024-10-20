05631116d279d5fa7b322b5204f0d236c66e87f6
encrypted-oreo <181586123+encrypted-oreo@users.noreply.github.com>
2024-10-20 17:07:17 +0100

    Create take_a_ten_minutes_walk.py

--- 6kyu/take_a_ten_minutes_walk.py
+++ 6kyu/take_a_ten_minutes_walk.py
@@ -1,3 +1,4 @@
 # https://www.codewars.com/kata/54da539698b8a2ad76000228
 
-is_valid_walk = lambda walk: len(walk) == 10 and walk.count("n") == walk.count("s") and walk.count("w") == walk.count("e")
+def is_valid_walk(walk):
+  return len(walk) == 10 and walk.count("n") == walk.count("s") and walk.count("w") == walk.count("e")
